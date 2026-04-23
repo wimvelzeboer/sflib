87144e25233938744f161ae249f3c5f058c03c07
William Velzeboer <6429417+wimvelzeboer@users.noreply.github.com>
2026-04-23 08:54:20 +0100

    Feature/record-actions
    The feature allows you to offer a list of actions to users on a record.
    
    A record action is an action that can be performed on a record, by invoking a screen-flow. Listed actions are filtered based on a configured set of criteria.

---
+++ docs/feature/record-actions.md
@@ -0,0 +1,159 @@
+# Record Actions
+
+The record actions feature allows you to offer a list of actions to users on a record.
+
+A record action is an action that can be performed on a record, by invoking a screen-flow. Listed actions are filtered based on a configured set of criteria.
+
+The actions are displayed in a list or tiles view. It can contain an action menu at the top right corner, when there are items to show (eg. history is enabled or the current user is an record action manager).
+
+![List View](./docs/feature/record-action_list-view.png "List View")
+
+![tiles](./docs/feature/record-action_tile-view.png "Tiles View")
+
+
+__Contents__:
+- [Configurational options](#configurational-options)
+- [Database Schema](#database-schema)
+- [LWC Component](#lwc-components)
+- [Permissions](#permissions)
+- [Dependencies](#dependencies)
+- [Limitations](#limitations)
+
+## Configurational options
+Record actions can be configured to in the `Record Actions Setup` App.
+
+![Record Actions Setup](./docs/feature/record-action_setup.png "Record Actions Setup")
+
+This app holds the record actions, offered by the SFLib Record Action feature and the record action settings.
+Tabs:
+ - [Record Actions](#manage-record-actions),<br/>Where actions can be configured 
+ - [Record Action Settings](#manage-record-action-settings)<br/>To manage Record Action settings
+
+### Manage Record Actions
+
+![Record Actions Setup Detail](./docs/feature/record-action_setup-detail.png "Record Actions Setup Detail")<br/>
+_Actions Detail page of the 'Record Actions Setup' Lightning App._
+
+Showing actions based on a formula with conditions like
+    - `1 AND 2`
+    - 1: `Case.Status = 'New'`
+    - 2: `Case.Priority = 'High'`
+
+### Manage Record Action Settings
+
+![Record Actions Setup Settings](./docs/feature/record-action_setup-settings.png "Record Actions Setup Settings")<br/>
+_Configurable settings can be managed under the settings tab_
+
+## Database Schema
+```mermaid
+erDiagram
+    sflib_RecordAction__c {
+        string Id PK
+        string Name
+        string Description__c
+        string FlowName__c
+        string Icon__c
+        string Status__c
+        string SObjectType__c
+        string Formula__c
+        decimal Order__c
+    }
+    
+    sflib_RecordActionCondition__c {
+        string Id PK
+        string RecordAction__c FK
+        decimal Number__c
+        string Formula__c
+        string SObjectType__c
+    }
+
+    sflib_RecordActionHistory__c {
+        string Id PK
+        string RecordId__c
+        string RecordAction__c FK
+        string CreatedDate__c
+        string CreatedBy__c
+        string Status__c
+        string message__c
+    }
+
+    sflib_RecordAction__c ||--o{ sflib_RecordActionCondition__c : ActionId__c
+    sflib_RecordAction__c ||--o{ sflib_RecordActionHistory__c : ActionId__c
+```
+| Table                            | Description                    |
+|----------------------------------|--------------------------------|
+| `sflib_RecordAction__c`          | The record actions             |
+| `sflib_RecordActionCondition__c` | The conditions for the actions |
+| `sflib_RecordActionHistory__c`   | The history of the actions     |
+
+| Field                              | Example Values                                |
+|------------------------------------|-----------------------------------------------|
+| `Action__c.Formula__c`             | `1 OR 2 AND 3`                                |
+| `Action__c.Status__c`              | `Draft`, `Active`, `Disabled`                 |
+| `ActionCondition__c.Formula__c`    |                                               |
+| `ActionCondition__c.ObjectName__c` | `Case`, `Account`, `Contact`, `User`          |
+| `ActionHistory__c.Status__c`       | `Started`, `Completed`, `Cancelled`, `Failed` |
+
+
+
+## LWC Components
+
+### Record Action 
+`c-sflib-record-actions`
+
+A list of actions that meet the configured criteria.
+
+__For Use In__
+
+Lightning Experience Components, Lightning Pages, Standaline Lightning App
+
+#### Attributes
+| Attribute   | Description                                                                             | Type   | Default          | Required |
+|-------------|-----------------------------------------------------------------------------------------|--------|------------------|----------|
+| `record-id` | The record Id for which to show the actions                                             | String |                  | Yes      |
+| `title`     | The title of the component                                                              | String | `Actions`        |
+| `variant`   | The variant type of how the component displays.<br/>Valid values are `list` and `tiles` | Enum   | `list`           |
+| `icon-name` | The name of the icon to display                                                         | String | `custom:custom9` | 
+
+#### Methods
+The component has no methods.
+
+### Example
+```html
+<template>
+    <c-sflib-record-actions
+         record-id={recordId}
+         variant="tiles"
+    ></c-sflib-record-actions>
+</template>
+```
+
+![tiles](./docs/feature/record-action_tile-view.png "Tiles View")
+
+
+## Permissions
+There are two permission sets for the Record Actions feature.
+- Record Action User `sflib_RecordActionUSer`
+- Record Action Administrator `sflib_RecordActionAdmin`
+
+The user permissions set enables the following actions:
+- 'execute' Record actions and list action history
+- `View` on `sflib_RecordAction__c`
+- `View` on `sflib_RecordActionCondition__c`
+- `View` on `sflib_RecordActionHistory__c`
+
+Administrators have access to the 'Record Actions Setup' App 
+where they are able to perform the following actions:
+- `CRUD` on `sflib_RecordAction__c` and `sflib_RecordActionCondition__c`
+- `Edit` on `sflib_RecordActionHistory__c`
+- `Edit` on settings (using the Settings Tab) 
+
+## Dependencies
+This extension package is dependent on the following packages:
+- [fflib-apex-mocks](https://github.com/apex-enterprise-patterns/fflib-apex-mocks)
+- [fflib-apex-common](https://github.com/apex-enterprise-patterns/fflib-apex-common)
+- [fflib-apex-extensions](https://github.com/wimvelzeboer/fflib-apex-extensions)
+- System.FormulaEval, used to evaluate the conditions in `Formula__c`
+
+## Limitations
+- Only supports screen flows.
---
+++ force-app/RecordActions/default/lwc/sflibRecordAction/sflibRecordAction.js
@@ -0,0 +1,410 @@
+/**
+ * SflibRecordAction LWC Component
+ *
+ * LWC component to display a single record action that can be performed on a record, by invoking a screen-flow.
+ * Listed actions are filtered based on a configured set of criteria.
+ *
+ * @author architect ir. Wilhelmus G.J. Velzeboer
+ *
+ * Copyright (c), W.G.J. Velzeboer,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ *   are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above author notice,
+ *      this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ * - Neither the name of the author nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @see SflibRecordAction
+ *
+ * @example
+ * <c-sflib-record-action
+ *      record-id={recordId}
+ *      action-id={actionId}
+ *      variant="list"
+ * ></c-sflib-record-actions>
+ */
+import {api, LightningElement, wire} from 'lwc';
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+import getSettings from '@salesforce/apex/sflib_RecordActionConfigController.getConfig';
+import logAction from '@salesforce/apex/sflib_RecordActionHistoryController.logAction';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+import DESCRIPTION_FIELD from '@salesforce/schema/sflib_RecordAction__c.Description__c';
+import FLOW_FIELD from '@salesforce/schema/sflib_RecordAction__c.FlowName__c';
+import ICON_FIELD from '@salesforce/schema/sflib_RecordAction__c.Icon__c';
+import NAME_FIELD from '@salesforce/schema/sflib_RecordAction__c.Name';
+
+const FIELDS = [NAME_FIELD, DESCRIPTION_FIELD, ICON_FIELD, FLOW_FIELD];
+
+export default class SflibRecordAction extends LightningElement {
+
+    /**
+     * The `sflib_RecordAction__c.Id` value
+     *
+     * @type {string}
+     * @public
+     * @required
+     */
+    @api actionId;
+
+    /**
+     * The record Id used while executing the action
+     *
+     * @type {string}
+     * @public
+     * @required
+     */
+    @api recordId;
+
+    /**
+     * The variant of how the action is displayed
+     * @type {'list'|'tiles'}
+     * @public
+     * @default 'list'
+     */
+    @api variant;
+
+    /**
+     * The state of the action
+     * @type {'idle'|'running'|'failed'|'completed'}
+     * @public
+     * @default 'idle'
+     */
+    @api
+    set state(value) {
+        this._state = value;
+        if (this._state !== 'idle' && this._state !== 'running' && this._state !== 'failed' && this._state !== 'completed') {
+            console.log('Defaulting state to Idle, ' + this._state);
+            this._state = 'idle';
+        }
+    };
+    get state() {
+        return this._state;
+    };
+
+    /**
+     * Indicates whether the description is collapsed
+     * @type {boolean}
+     */
+    collapsed = true;
+
+    /**
+     * Indicates whether the description is clamped
+     * @type {boolean}
+     */
+    isClamped = false;
+
+    /**
+     * The `sflib_RecordAction__c` record
+     * @type {sflib_RecordAction__c}
+     */
+    record = {};
+
+    /**
+     * Indicates whether the modal is shown
+     * @type {boolean}
+     */
+    showModal = false;
+
+    /**
+     * Indicates whether the description is truncated and a button to expand is shown
+     * @type {boolean}
+     */
+    showReadMore = false;
+
+    /**
+     * The Id of the sflib_RecordActionHistory__c record that is created when the action is executed.
+     * @type {string|null}
+     * @private
+     */
+    _actionLogId = null;
+
+    /**
+     * Holds the settings for the Record Actions feature
+     * @type {{historyLog: boolean}}
+     * @private
+     */
+    _settings = {
+        historyLog: false
+    };
+
+    /**
+     * The state of the action
+     * @type {'idle'|'running'|'failed'|'completed'}
+     * @private
+     */
+    _state = 'idle';
+
+    /**
+     * Loads the actions settings
+     */
+    @wire(getSettings, {})
+    getWiredSettings({error, data}) {
+        if (error) {
+            console.error(error);
+        }
+        if (data) {
+            // reload actions whenever the record changes
+            this._settings = data;
+        }
+    }
+
+    /**
+     * Wires the record to the component
+     */
+    @wire(getRecord, {recordId: '$actionId', fields: FIELDS})
+    wiredRecordAction({error, data}) {
+        if (data) {
+            this.record = data;
+        } else if (error) {
+            console.error('Error retrieving account:', error);
+        }
+    }
+
+    /**
+     * Adds a new sflib_RecordActionHistory__c record to the system.
+     * The record is created with the current user as the owner.
+     * @param status {'Started' | 'Completed' | 'Failed' | 'Cancelled'}
+     * @param callback {function} Optional callback function to be executed after the record is created.
+     */
+    addHistoryLog(status, callback = null) {
+        logAction({actionLogId: this._actionLogId, recordId: this.recordId, actionId: this.actionId, status: status})
+            .then(result => {
+                this._actionLogId = result;
+                if (callback !== null) {
+                    callback();
+                }
+            })
+            .catch(error => {
+                console.error('Error logging action: ' + error);
+            });
+    }
+
+    get buttonTitle() {
+        if (this.state === 'failed') {
+            return 'Retry - ' + this.title;
+        } else if (this.state === 'completed') {
+            return 'Rerun - ' + this.title;
+        } else if (this.state === 'running') {
+            return 'Restart -  ' + this.title + ' (running)';
+        } else {
+            return this.title;
+        }
+    }
+
+    get buttonVariant() {
+        if (this.state === 'failed') {
+            return 'destructive';
+        } else if (this.state === 'completed') {
+            return 'success';
+        } else if (this.state === 'running') {
+            return 'brand-outline';
+        } else {
+            return 'brand';
+        }
+    }
+
+    /**
+     * Returns the description of the action
+     * @returns {string|null}
+     */
+    get description() {
+        return this.record ? getFieldValue(this.record, DESCRIPTION_FIELD) : null;
+    }
+
+    /**
+     * Returns the flow name of the action
+     * @returns {string|null}
+     */
+    get flowName() {
+        return this.record ? getFieldValue(this.record, FLOW_FIELD) : null;
+    }
+
+    /**
+     * Returns the flow variables of the action
+     * @returns {Object[]}
+     */
+    get flowVariables() {
+        return [
+            {name: 'recordId', type: 'String', value: this.recordId}
+        ];
+    }
+
+    /**
+     * Returns the icon name of the action
+     * @returns {string|null}
+     */
+    get iconName() {
+        let result = this.record ? getFieldValue(this.record, ICON_FIELD) : null;
+        return result === null || result === undefined ? 'custom:custom9' : result;
+    }
+
+    /**
+     * Returns true if the variant is tiles
+     * @returns {boolean}
+     */
+    get showTiles() {
+        return this.variant === 'tiles';
+    }
+
+    get startButtonTitle() {
+        if (this.state === 'failed') {
+            return 'Retry Action';
+        } else if (this.state === 'completed') {
+            return 'Rerun Action';
+        } else if (this.state === 'running') {
+            return 'Restart Action';
+        } else {
+            return 'Start Action';
+        }
+    }
+
+    /**
+     * Returns the title of the action
+     * @returns {string|null}
+     */
+    get title() {
+        return this.record ? getFieldValue(this.record, NAME_FIELD) : null;
+    }
+
+    /**
+     * Handles the click event of the action
+     */
+    handleClick() {
+        let flowName = getFieldValue(this.record, FLOW_FIELD);
+        if (flowName === undefined || flowName === null) {
+            this.showError('There is no flow assigned to the selected action');
+            return;
+        }
+        this.showMessage('Info', 'info', 'Running flow: ' + flowName);
+
+        if (this._settings.historyLog) {
+            this.addHistoryLog('Started', () => this.showModal = true);
+        } else {
+            this.showModal = true;
+        }
+    }
+
+    /**
+     * Handles the close event of the modal
+     */
+    handleCloseModal() {
+        this.showModal = false;
+    }
+
+    /**
+     * Handles the flow status change event
+     * @param event
+     */
+    handleFlowStatusChange(event) {
+        // Handle flow status changes
+        if (event.detail.status === 'FINISHED_SCREEN') {
+            this.showMessage('Success', 'success', 'Flow finished successfully');
+            this.showModal = false;
+
+            if (this._actionLogId !== undefined && this._actionLogId !== null) {
+                this.addHistoryLog('Completed',
+                    () => this.publishStatusChange(event.detail.status));
+            } else {
+                this.publishStatusChange(event.detail.status);
+            }
+        } else if (event.detail.status === 'ERROR') {
+            if (this._actionLogId !== undefined && this._actionLogId !== null) {
+                this.addHistoryLog('Failed');
+            }
+            this.showError(event.detail.errors[0].message);
+            this.showModal = false;
+        }
+    }
+
+    /**
+     * Handles the click event of the read more/less button
+     */
+    handleToggleReadMore() {
+        const textElement = this.template.querySelector('.content');
+        const button = this.template.querySelector('.read-more-btn');
+
+        if (this.collapsed) {
+            // Expand: Remove clamp
+            textElement.style.webkitLineClamp = 'none';
+            textElement.style.display = 'block'; // Or remove class
+            button.textContent = 'Read Less';
+            this.collapsed = false;
+        } else {
+            // Collapse: Re-apply clamp
+            textElement.style.webkitLineClamp = '4';
+            textElement.style.display = '-webkit-box';
+            button.textContent = 'Read More';
+            this.collapsed = true;
+        }
+    }
+
+    /**
+     * Publishes the status change event to the parent component
+     * @param status
+     */
+    publishStatusChange(status) {
+        const myEvent = new CustomEvent('changestatus', {
+            detail: {data: status},
+        });
+        this.dispatchEvent(myEvent);
+    }
+
+    /**
+     * Checks if the description is clamped and shows the read more button if needed
+     */
+    renderedCallback() {
+        if (this.isClamped) return; // Prevent re-calculation
+
+        const textElement = this.template.querySelector('.content');
+        if (textElement) {
+            if (textElement.offsetHeight < textElement.scrollHeight) {
+                this.isClamped = true;
+                this.showReadMore = true;
+            }
+        }
+    }
+
+    /**
+     * Shows an error message
+     *
+     * @param message
+     */
+    showError(message) {
+        this.showMessage('Error', 'error', message);
+    }
+
+    /**
+     * Shows a toast message
+     *
+     * @param title {string}
+     * @param variant {'info' | 'success' | 'warning' | 'error'}
+     * @param message {string}
+     */
+    showMessage(title, variant, message) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: message,
+                variant: variant,
+                mode: 'dismissable'
+            })
+        );
+    }
+}
